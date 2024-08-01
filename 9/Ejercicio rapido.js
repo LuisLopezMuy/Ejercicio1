46d442d36ce4c61e16cf01bd72a5590e1053c324
LuisLopezMuy <luislopezmuy2@gmail.com>
2024-07-31 20:32:15 -0600

    Ejercicios clase 9

---
+++ 9/Ejercicio rapido.js
@@ -0,0 +1,12 @@
+var arr = [1,2,3,4,5];
+console.log(arr);
+
+arr.push(6);
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])    
+};
